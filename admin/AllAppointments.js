306814ae6d24ba6db4952b61fbbbaffc7858421a
Hicham Bendarbi <49118568+hichambendarbi@users.noreply.github.com>
2020-10-06 11:48:50 +0100

    adaptation table rdv

--- admin/AllAppointments.js
+++ admin/AllAppointments.js
@@ -1,8 +1,7 @@
 import React, {useState} from 'react'
-import './style.css'
 import styled from 'styled-components'
 
-const Ittyni = () => {
+const AppTest = () => {
     const [dateSearch, setdataSearch] = useState ({
         date: ''
     })
@@ -75,11 +74,11 @@ const Ittyni = () => {
     const body = Test.map(appo => (
         <tbody>
         <tr>
-            <td data-column="First Name">{appo.name}</td>
-            <td data-column="Last Name">{appo.phone}</td>
-            <td data-column="Job Title">{appo.date}</td>
-            <td data-column="Twitter">{appo.time}</td>
-            <td data-column="Twitter">{appo.text}</td>
+            <Td data-column="Client">{appo.name}</Td>
+            <Td data-column="Telephone ">{appo.phone}</Td>
+            <Td data-column="Date">{appo.date}</Td>
+            <Td data-column="Heure">{appo.time}</Td>
+            <Td data-column="Text">{appo.text}</Td>
         </tr>
        </tbody>
     ))
@@ -92,11 +91,11 @@ const Ittyni = () => {
                         <table>
                             <thead>
                                 <tr>
-                                <th>Client</th>
-                                <th>Telephone de client</th>
-                                <th>Date de rendez-vous</th>
-                                <th>Heure de rendez-vous</th>
-                                <th>Text de rendez-vous</th>
+                                <Th>Client</Th>
+                                <Th>Telephone </Th>
+                                <Th>Date</Th>
+                                <Th>Heure</Th>
+                                <Th>Text</Th>
                                 </tr>
                             </thead>
                             {body}
@@ -105,7 +104,7 @@ const Ittyni = () => {
     )
 }
 
-export default Ittyni
+export default AppTest
 
 const ContainerAppointment = styled.div`
 display: flex;
@@ -113,12 +112,49 @@ justify-content: center;
 margin-top: 20px;
 flex-direction: column;
 margin: 20px 100px;
+@media(max-width: 880px) {
+    margin: 20px 10px;
+}
+
 `
 
 const SearchInput = styled.input`
-height: 30px;
-width: 25%;
+width: 40%;
 height: 35px;
 padding-left: 5px;
 margin-left: 2px;
+border: 1px solid red;
+border-radius: 5px;
+margin-bottom: 5px;
+`
+
+const Th = styled.th`
+background: #014f00;
+color: #FFF; 
+font-weight: bold; 
+padding: 10px; 
+border: 1px solid #ccc; 
+text-align: center; 
+font-size: 16px;
+@media(max-width: 1000px) {
+    font-size: 12px;
+}
+
+@media(max-width: 880px) {
+    font-size: 10px;
+}
+`
+
+const Td = styled.td`
+padding: 10px; 
+border: 1px solid #ccc; 
+text-align: center; 
+font-size: 16px;
+@media(max-width: 1000px) {
+    font-size: 12px;
+}
+
+@media(max-width: 880px) {
+    font-size: 10px;
+}
 `
